567ac2608182a75c1f4cbce347e747e21f2eb7bd
Gutoovelha <48135676+Gutoovelha@users.noreply.github.com>
2019-03-01 16:04:18 -0300

    Add files via upload

---
+++ 1_Exercicio.php
@@ -0,0 +1,11 @@
+<?php
+ //Faça um programa q dado duas 
+ //variaveis, imprima a soma
+
+    $numero1 = 10;
+    $numero2 = 5;
+
+    $soma = $numero1 + $numero2;
+
+    print"A soma dos valores é $soma \n";
+
