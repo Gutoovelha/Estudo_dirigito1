a966cc92ff5ca027a5cc9908d98c1f1b5fd7cd92
Gutoovelha <48135676+Gutoovelha@users.noreply.github.com>
2019-03-01 16:57:08 -0300

    Add files via upload

---
+++ Marceneiro.php
@@ -0,0 +1,29 @@
+<?php
+
+
+  $sobra5m = 500 % 45;
+  $divisao5m = (500-$sobra5m)/ 45;
+
+ print " Irá sobrar da tabua de madeira q ele comprou, $sobra5m cm \n ";
+ print "Ele pode cortar $divisao5m cm de madeira sem sobrar\n\n";
+
+
+
+
+ $sobra4m = 400 % 45;
+ $divisao4m = (400-$sobra4m)/ 45;
+
+print " Irá sobrar da tabua de madeira q ele comprou, $sobra4m cm \n ";
+print "Ele pode cortar $divisao4m cm de madeira sem sobrar\n\n";
+
+
+
+
+ $sobra3m = 300 % 45;
+ $divisao3m = (300-$sobra3m)/ 45;
+
+ print " Irá sobrar da tabua de madeira q ele comprou, $sobra3m cm \n ";
+ print "Ele pode cortar $divisao3m cm de madeira sem sobrar\n\n";
+
+
+
