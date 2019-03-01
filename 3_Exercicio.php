c7faf6fc2bcf16c4d5800f10a6642b89575bc677
Gutoovelha <48135676+Gutoovelha@users.noreply.github.com>
2019-03-01 16:03:29 -0300

    Add files via upload

---
+++ 3_Exercicio.php
@@ -0,0 +1,12 @@
+<?php
+
+ //escreva um programa para ler dois numeros e trocar seus valores.
+    $a = 33;
+    $b = 99;
+
+ //troca 
+ $auxuliar = $a;
+ $a = $b;
+ $b = $auxuliar;
+
+ print"O valor de \$a é $a e o valor de \$b é $b";
