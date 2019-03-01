cc3a07a784152204906e7458f6a7bdaad06b1438
Gutoovelha <48135676+Gutoovelha@users.noreply.github.com>
2019-03-01 16:14:05 -0300

    Add files via upload

---
+++ 2_Exercicio.php
@@ -0,0 +1,8 @@
+<?php       
+
+    $nota_primeiro = 9;
+    $nota_segunfo  = 9;
+    $nota_terceiro = 9;
+    $nota_quarto   = 6;
+
+    $media = ( $nota_primeiro + $nota_segunfo + $nota_terceiro + $nota_quarto)/4;
